3c0a33e10022961bbae8165264cd9976dff17c60
Saeed Hadadan <saeed@zwicker-computer.cs.umd.edu>
2023-07-03 15:43:53 -0400

    edit scripts

--- sample_scripts/all_scenes/dining_room.sh
+++ sample_scripts/all_scenes/dining_room.sh
@@ -1,6 +1,4 @@
 bash sample_scripts/all_scenes/run_nerad.sh \
 dataset.scene=data/NeRad_paper_scenes/dining_room/scene.xml \
 name=dining_room \
-rendering/pos_emb=dense_grid \
-rendering.pos_emb.resolution=512 \
 ${@}
